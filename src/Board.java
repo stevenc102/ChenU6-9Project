b7601d3bb78091f7f28bafbb9398d6b2aa97aeae
student <sample@bths.edu>
2023-03-29 11:27:26 -0400

    commit

--- src/Board.java
+++ src/Board.java
@@ -9,7 +9,66 @@ public class Board {
         board = new int[height][width];
     }
 
+    public void initBoard() {
+        int bombs = (int)(rows * columns * 0.21);
+        while(bombs > 0) {
+            for (int i = 0 ; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if ((int)(Math.random() * 8) == 0 && bombs > 0) {
+                        board[i][j] = 9;
+                        bombs--;
+                    }
+                }
+            }
+        }
+        for (int i = 0 ; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 9) {
+                    incrementAdjacent(i, j);
+                }
+            }
+        }
+    }
 
+    public int getHeight() {
+        return rows;
+    }
+
+    public int getWidth() {
+        return columns;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    private void incrementAdjacent(int row, int col) {
+        int r = row;
+        int c = col;
+        int startR = r - 1;
+        int endR = r + 1;
+        int startC = c - 1;
+        int endC = c + 1;
+        if (startR < 0) {
+            startR = 0;
+        }
+        if (endR > board.length - 1) {
+            endR = board.length - 1;
+        }
+        if (startC < 0) {
+            startC = 0;
+        }
+        if (endC > board[0].length - 1) {
+            endC = board[0].length - 1;
+        }
+        for (int i = startR; i <= endR; i++) {
+            for (int j = startC; j <= endC; j++) {
+                if (board[i][j] != 9) {
+                    board[i][j] ++;
+                }
+            }
+        }
+    }
 
 
 }
--- src/Game.java
+++ src/Game.java
@@ -1,33 +1,48 @@
-import javax.swing.*;
-import java.awt.*;
 
-public class Game extends JPanel {
-    private int HEIGHT = 200;
-    private int WIDTH = 200;
-
+public class Game{
     private Board board;
+    private Board revealedBoard;
     public Game() {
-        Board gameBoard = new Board(16, 16);
-        JFrame frame = new JFrame("Minesweeper");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(HEIGHT, WIDTH);
-        frame.setVisible(true);
-        GridLayout grid = new GridLayout(2, 2);
-        int count = 1;
-        for (int i = 0 ; i < 4; i++) {
 
-            Button button = new Button(count + "");
-            button.setBounds(new Rectangle(5, 5));
-            count++;
-            frame.add(button);
+        board = new Board(16, 16);
+        revealedBoard = new Board(16, 16);
+        board.initBoard();
 
+
+    }
+
+    public void initGame(){
+        System.out.print("  ");
+        for (int width = 0; width < revealedBoard.getBoard()[0].length; width++){
+            System.out.print("|---");
+        }
+        System.out.println(" |");
+        for (int i = 0; i < revealedBoard.getBoard().length; i++){
+            if (i <= 10){
+                System.out.print((i+1) + " |");
+            } else {
+                System.out.print((i + 1) +"|");
+            }
+            for (int j = 0; j < revealedBoard.getBoard()[i].length; j++){
+                if (revealedBoard.getBoard()[i][j] == 9) {
+                    System.out.print("💣 |");
+                } else if (revealedBoard.getBoard()[i][j] == 0){
+                    if (i < 10){
+                        System.out.print("   " + "|");
+                    } else {
+                        System.out.print("   " + " |");
+                    }
+                } else {
+                    System.out.print(" " + revealedBoard.getBoard()[i][j] + " |");
+                }
+            }
+            System.out.println("");
+            System.out.print("  ");
+            for (int width = 0; width < revealedBoard.getBoard()[0].length; width++){
+                System.out.print("|---");
+            }
+            System.out.println(" |");
         }
 
-        frame.setLayout(grid);
     }
-
-    public Board getBoard() {
-        return board;
-    }
-
 }
