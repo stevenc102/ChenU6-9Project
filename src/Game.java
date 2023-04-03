68803a52204641355079107e019aef6ef42f467d
aaa <aaa@>
2023-04-03 19:38:38 -0400

    done?

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,8 @@
 public class Game{
     private final Board board;
     public Game() {
-
         board = new Board(16, 16);
-
         board.initBoard();
-
-
     }
 
     public Board getBoard() {
