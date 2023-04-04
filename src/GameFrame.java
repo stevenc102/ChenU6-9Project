8e40e1c44bff2bcb984b7bafaa5ad4a253781d16
aaa <aaa@>
2023-04-03 20:22:19 -0400

    done?

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,13 +3,15 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
 public class GameFrame extends JFrame implements ActionListener {
     private Game minesweeper;
     private GamePanel gamePanel;
     private Timer time;
     private boolean temp;
     private JFrame frame;
-    private static int highScore = 0;
+    private static ArrayList<Integer> highScore = new ArrayList<Integer>();
     private int seconds;
     private boolean restart;
     private JButton restartButton;
@@ -59,8 +61,8 @@ public class GameFrame extends JFrame implements ActionListener {
             frame.setLayout(new GridLayout(2, 2));
             frame.add(new JLabel("YOU LOSE", SwingConstants.CENTER));
             frame.add(new JLabel("Time taken: " + seconds, SwingConstants.CENTER));
-            if (highScore != 0) {
-                frame.add(new JLabel("Current highscore: " + highScore + " seconds", SwingConstants.CENTER));
+            if (highScore.size() != 0) {
+                frame.add(new JLabel("Current highscores: " + highScore + " seconds", SwingConstants.CENTER));
             } else {
                 frame.add(new JLabel("There is currently no highscore. ", SwingConstants.CENTER));
             }
@@ -69,15 +71,15 @@ public class GameFrame extends JFrame implements ActionListener {
             temp = false;
         } else if (gamePanel.checkWon() && temp){
             gamePanel.endGame();
-            highScore = seconds;
+            highScore.add(seconds);
             frame.setSize(600, 400);
             frame.setTitle("Minesweeper");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new GridLayout(2, 2));
             frame.add(new JLabel("YOU WIN", SwingConstants.CENTER));
             frame.add(new JLabel("Time taken: " + seconds, SwingConstants.CENTER));
-            if (highScore != 0) {
-                frame.add(new JLabel("Current highscore: " + highScore + " seconds", SwingConstants.CENTER));
+            if (highScore.size() != 0) {
+                frame.add(new JLabel("Current highscores: " + highScore + " seconds", SwingConstants.CENTER));
             } else {
                 frame.add(new JLabel("There is currently no highscore. ", SwingConstants.CENTER));
             }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.util.ArrayList;
 
 public class GamePanel extends JPanel{
 
