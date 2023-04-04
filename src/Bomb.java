0a69b65120ebd5fc493951c35258243b5001de2f
aaa <aaa@>
2023-04-03 20:35:23 -0400

    done

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,6 +1,9 @@
+import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+
 public class Bomb extends Tile implements ActionListener{
     private boolean lost;
     public Bomb(int row, int col) {
@@ -14,11 +17,15 @@ public class Bomb extends Tile implements ActionListener{
 
 
     public void actionPerformed(ActionEvent ae){
-        getButton().setEnabled(false);
-        getButton().setBackground(Color.red);
-        getButton().setText("💣");
-        lost = true;
+        if (!getFlagged()) {
+            getButton().setEnabled(false);
+            getButton().setBackground(Color.WHITE);
+            getButton().setText("💣");
+            lost = true;
+        }
     }
+
+
 
     public void setDisabled() {
        getButton().setEnabled(false);
